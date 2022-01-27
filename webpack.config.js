ed0ee676e3cd3b618d3b7d86a91d609be350b4d3
Brandon A <byarrieta@protonmail.com>
2022-01-26 23:10:08 -0500

    webpack JS

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path') //path esta disponible en node, no hay que instalar
+
+module.exports = {
+
+  // punto de entrada app
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),  //dist estandar, aca se guarda el proyecto
+    filename: "main.js" //Js resultante,
+  },
+
+  resolve: {
+    extensions: ['.js']
+  },
+
+  module: {
+    rules: [
+      {
+        // Test declara que extensión de archivos aplicara el loader
+        test: /\.m?js$/,
+        // Use es un arreglo u objeto donde dices que loader aplicaras
+        use: {
+          loader: "babel-loader"
+        },
+        // Exclude permite omitir archivos o carpetas especificas
+        exclude: /node_modules/
+      }
+    ]
+  }
+
+}
