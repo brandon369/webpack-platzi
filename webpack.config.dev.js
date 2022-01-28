0e7fd077336711aacd2e8d6801010c1f175c0208
Brandon A <byarrieta@protonmail.com>
2022-01-27 23:34:22 -0500

    bundle analyzer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const DotEnv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 
 module.exports = {
 
@@ -82,7 +83,8 @@ module.exports = {
         }
       ]
     }),
-    new DotEnv()
+    new DotEnv(),
+    new BundleAnalyzerPlugin()
   ],
   // devServer: {
   //   codeBase: path.join(__dirname, 'dist'),
